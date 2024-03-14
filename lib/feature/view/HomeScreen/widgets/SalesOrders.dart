ee41785f1e4c118001a273ed9a1e9968c34a1331
Rishana-TS <rishana@turbosoft.technology>
2024-03-14 12:06:55 +0530

    test push

--- lib/feature/view/HomeScreen/widgets/SalesOrders.dart
+++ lib/feature/view/HomeScreen/widgets/SalesOrders.dart
@@ -1,3 +1,4 @@
+import 'package:customer_connect/feature/view/totalorders/totalordersheader.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -8,7 +9,7 @@ class SalesOrders extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
           width: MediaQuery.of(context).size.width,
@@ -19,99 +20,123 @@ class SalesOrders extends StatelessWidget {
               gradient: const LinearGradient(
                 begin: Alignment.topRight,
                 end: Alignment.bottomLeft,
-                colors: [
-                  Color(0xffCEE5C6),
-                  Color(0xffEAF6E6)
-                ],
-              )
-          ),
+                colors: [Color(0xffCEE5C6), Color(0xffEAF6E6)],
+              )),
           child: Column(
             children: [
               Padding(
-                padding: const EdgeInsets.only(left: 20.0,right: 20,top: 10,bottom: 10),
+                padding: const EdgeInsets.only(
+                    left: 20.0, right: 20, top: 10, bottom: 10),
                 child: Row(
                   // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text("Sales Orders",style: boxHeading(),),
+                    Text(
+                      "Sales Orders",
+                      style: boxHeading(),
+                    ),
                     //  Text("33"),
                   ],
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 05.0,right: 05),
-                child:Row(
+                padding: const EdgeInsets.only(left: 05.0, right: 05),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const TotalOrders()));
+                      },
+                      child: Container(
+                        height: 60,
+                        width: 160,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white),
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 12.0, top: 5, bottom: 5),
+                          child: Row(
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SvgPicture.asset(
+                                    "assets/svg/orders.svg",
+                                    height: 25,
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text("10", style: countHeading()),
+                                  Text("AED 0.00", style: subTextStyle()),
+                                  Text(
+                                    "Total Order",
+                                    style: statusTextStyle(),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
                     Container(
                       height: 60,
                       width: 160,
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
-                          color: Colors.white
-                      ),
+                          color: Colors.white),
                       child: Padding(
-                        padding: const EdgeInsets.only(left:12.0,top: 5,bottom: 5),
+                        padding: const EdgeInsets.only(
+                            left: 12.0, top: 5, bottom: 5),
                         child: Row(
                           children: [
                             Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                SvgPicture.asset("assets/svg/orders.svg",height: 25,),
+                                SvgPicture.asset(
+                                  "assets/svg/tdel.svg",
+                                  height: 20,
+                                ),
                               ],
                             ),
-                            const SizedBox(width: 10,),
+                            const SizedBox(
+                              width: 10,
+                            ),
                             Column(
                               mainAxisAlignment: MainAxisAlignment.start,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text("10",style: countHeading()),
-                                Text("AED 0.00",style: subTextStyle()),
-                                Text("Total Order",style: statusTextStyle(),)
+                                Text("0/0", style: countHeading()),
+                                Text("AED 0.00", style: subTextStyle()),
+                                Text(
+                                  "Today's Deliveries",
+                                  style: statusTextStyle(),
+                                )
                               ],
                             ),
                           ],
                         ),
                       ),
                     ),
-                    const SizedBox(width: 5,),
-                    Container(
-                      height: 60,
-                      width: 160,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 12.0,top: 5,bottom: 5),
-                        child: Row(
-                          children: [
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SvgPicture.asset("assets/svg/tdel.svg",height: 20,),
-                              ],
-                            ),
-                            const SizedBox(width: 10,),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("0/0",style: countHeading()),
-                                Text("AED 0.00",style: subTextStyle()),
-                                Text("Today's Deliveries",style: statusTextStyle(),)
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-
                   ],
                 ),
               ),
             ],
-          )
-      ),
+          )),
     );
   }
 }
